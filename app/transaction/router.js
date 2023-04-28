94383c511f793854225370308b610be07a3d2d40
Redha <tataredhaalfath@gmail.com>
2023-04-28 13:33:21 +0700

    Menu Transaction

---
+++ app/transaction/router.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  index, actionStatus,
+} = require("./controller");
+
+router.get("/", index);
+router.put("/status", actionStatus);
+
+module.exports = router;
