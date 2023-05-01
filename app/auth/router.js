6ee8c10167a333a7bf62f10200222da6a6998c1a
Redha <tataredhaalfath@gmail.com>
2023-05-01 08:48:07 +0700

    api signin

--- app/auth/router.js
+++ app/auth/router.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const multer = require("multer");
 const os = require("os");
 
-const { signup } = require("./controller");
+const { signup, signin } = require("./controller");
 
+// api
 router.post("/signup", multer({ dest: os.tmpdir() }).single("image"), signup);
-
+router.post("/signin", signin);
 module.exports = router;
