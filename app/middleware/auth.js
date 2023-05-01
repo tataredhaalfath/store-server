ca7d64a8b195b535686fbac5ff115f7137ae6e2d
Redha <tataredhaalfath@gmail.com>
2023-05-01 09:41:18 +0700

    api checkout

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -1,3 +1,8 @@
+const config = require("../../config");
+const jwt = require("jsonwebtoken");
+
+const Player = require("../player/model");
+
 module.exports = {
   isLoginAdmin: (req, res, next) => {
     if (req.session.user === null || req.session.user === undefined) {
@@ -9,6 +14,27 @@ module.exports = {
       res.redirect("/");
     } else {
       next();
+    }
+  },
+  isLoginPlayer: async (req, res, next) => {
+    try {
+      const token = req.headers.authorization
+        ? req.headers.authorization.replace("Bearer ", "")
+        : null;
+
+      const data = jwt.verify(token, config.jwtKey);
+      const player = await Player.findOne({ _id: data.player.id });
+      if (!player) {
+        throw new Error();
+      }
+
+      req.player = player;
+      req.token = token;
+      next();
+    } catch (err) {
+      res.status(401).json({
+        message: "Not authorized",
+      });
     }
   },
 };
