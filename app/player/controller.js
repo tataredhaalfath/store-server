764a53e29eeeac3ba4ec5e9fad7cc9e8900943f9
Redha <tataredhaalfath@gmail.com>
2023-05-01 08:34:57 +0700

    dropdown category

--- app/player/controller.js
+++ app/player/controller.js
@@ -1,5 +1,6 @@
 const Player = require("./model");
 const Voucher = require("../voucher/model");
+const Category = require("../category/model");
 
 module.exports = {
   index: async (req, res) => {
@@ -75,6 +76,26 @@ module.exports = {
 
       res.status(200).json({
         data: voucher,
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({
+        message: err.message || "Terjadi kesalahan pada server",
+      });
+    }
+  },
+
+  category: async (req, res) => {
+    try {
+      const category = await Category.find();
+      if (!category) {
+        return res.status(404).json({
+          message: "Voucher game tidak ditemukan!",
+        });
+      }
+
+      res.status(200).json({
+        data: category,
       });
     } catch (err) {
       console.log(err);
